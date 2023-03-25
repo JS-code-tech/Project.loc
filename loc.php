5b0258c6afc5f43d1778e9c3b7ceb3f6ddef5d93
LoneJL <javasj527@gmailcom>
2023-03-26 00:25:25 +0500

    boxes

--- loc.php
+++ loc.php
@@ -3,5 +3,17 @@
 
 $box = array("number4" => 4, "number6" => 6, "number8" => 8);
 
+
+$boxes = [ 'box' => '02', 'box2' > 'o3' ];
+
+
+echo $boxes['box'];
+
+
+echo "<pre>";
+
+
+
+
 print_r($box);
 
